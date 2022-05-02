191d1f5a96c7cc9a43dec19efcbd87e2a2e50c78
Peipi98 <atanasio.peppe@gmail.com>
2022-05-02 19:32:50 +0200

    add generics

--- lab5/lab5.py
+++ lab5/lab5.py
@@ -10,11 +10,13 @@ sys.path.append("/Users/peipi98/Documents/PoliTO/Materie/Machine Learning/labs/M
 from mlFunc import *
 
 def split_db_2to1(D, L, seed=0):
-    nTrain = int(D.shape[1]*2.0/3.0) 
+    nTrain = int(D.shape[1]*2.0/3.0)
     numpy.random.seed(seed)
     idx = numpy.random.permutation(D.shape[1])
     idxTrain = idx[0:nTrain]
     idxTest = idx[nTrain:]
+    print(idx)
+    print(idx.shape)
     
     DTR = D[:, idxTrain]
     DTE = D[:, idxTest]
@@ -23,16 +25,29 @@ def split_db_2to1(D, L, seed=0):
     
     return (DTR, LTR), (DTE, LTE)
 
-if __name__ == "__main__":
-    D, L = load_iris()
+def kfold_cross(D, L, k, seed=0):
+    nTrain = int(D.shape[1]*(k-1)/k)
+    numpy.random.seed(seed)
+    
+    idx = numpy.random.permutation(D.shape[1])
+    idx = numpy.array(idx)
+    #print(idx.shape)
+    idx = numpy.reshape(idx, (k, idx.shape[0]//k))
+    
+    return idx
+
+def evaluate_MVG(D, L):
     (DTR, LTR), (DTE, LTE) = split_db_2to1(D, L)
     
     h = {}
     
     for i in range(3):
         mu, C = ML_GAU(DTR[:, LTR==i])
-        h[i] = (mu, C)
+        # for Naive Bayes assumption deccoment:
+        # C = numpy.diag(numpy.diag(C))
         
+        h[i] = (mu, C)
+    
     SJoint = numpy.zeros((3, DTE.shape[1]))
     logSJoint = numpy.zeros((3, DTE.shape[1]))
     classPriors = [1.0/3.0, 1.0/3.0, 1.0/3.0]
@@ -58,3 +73,57 @@ if __name__ == "__main__":
     
     accuracy_2 = (LTE == LPred2).sum() /LTE.size
     error_2 = 1 - accuracy_2
+
+def evaluate_tied_GC(D, L):
+    (DTR, LTR), (DTE, LTE) = split_db_2to1(D, L)
+    # compute tied covariance matrix
+    N = numpy.shape(DTR)[1]
+    Ctied = numpy.zeros((4,4))
+    
+    h = {}
+    
+    for i in range(3):
+        _, Ctemp = ML_GAU(DTR[:, LTR==i])
+        Nc = numpy.shape(DTR[:, LTR==i])[1]
+        # for Naive Bayes assumption deccoment:
+        # C = numpy.diag(numpy.diag(C))
+        Ctied += Ctemp * Nc
+    Ctied /= N
+    
+    for i in range(3):
+        mu , _ = ML_GAU(DTR[:, LTR==i])
+        h[i] = (mu, Ctied)
+        
+    
+    SJoint_tied = numpy.zeros((3, DTE.shape[1]))
+    logSJoint_tied = numpy.zeros((3, DTE.shape[1]))
+    classPriors_tied = [1.0/3.0, 1.0/3.0, 1.0/3.0]
+    
+    for label in range(3):
+        mu, C = h[label]
+        
+        SJoint_tied[label, :] = numpy.exp(logpdf_GAU_ND(DTE, mu, C).ravel()) * classPriors_tied[label] 
+        logSJoint_tied[label, :] = logpdf_GAU_ND(DTE, mu, C).ravel() + numpy.log(classPriors_tied[label])
+        
+    SMarginal = SJoint_tied.sum(0)
+    logSMarginal = scipy.special.logsumexp(logSJoint_tied, axis=0)
+    
+    Post1 = SJoint_tied / mrow(SMarginal)
+    logPost = logSJoint_tied - mrow(logSMarginal)
+    Post2 = numpy.exp(logPost)
+    
+    LPred1 = Post1.argmax(0)
+    LPred2 = Post2.argmax(0)
+    
+    accuracy_1 = (LTE == LPred1).sum() /LTE.size
+    error_1 = 1 - accuracy_1
+    print(accuracy_1)
+    accuracy_2 = (LTE == LPred2).sum() /LTE.size
+    error_2 = 1 - accuracy_2
+        
+if __name__ == "__main__":
+    D, L = load_iris()
+    
+    #evaluate_MVG(D, L)
+    #evaluate_tied_GC(D, L)
+    print(kfold_cross(D, L, 3))
